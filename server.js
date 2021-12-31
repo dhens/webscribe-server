fb61227dcd1a5161370b5b487563cc1230520df8
Dawson Hensel <dawsonhensel@gmail.com>
2021-12-31 15:29:39 -0800

    Add catch all for other requests

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 80;
 app.use(bodyParser.text({type:"text/plain"}));
 
-app.post('/', function (req, res) {
+app.post("/", function(req, res) {
   console.log(req.body);
+
+  res.sendStatus(200);
+});
+
+app.all("*", function (req, res) {
   res.sendStatus(200);
 });
 
