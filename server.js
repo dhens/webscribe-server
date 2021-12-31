22b864fe707d91ba178b2e6b88d68225180f0e40
Dawson Hensel <dawsonhensel@gmail.com>
2021-12-31 14:06:14 -0800

    Make port deployment ready

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-
+const PORT = 80 || process.env.PORT;
 app.use(bodyParser.text({type:"*/*"}));
 
 app.post('/', function (req, res) {
@@ -9,6 +9,6 @@ app.post('/', function (req, res) {
   res.sendStatus(200);
 });
 
-app.listen(80, function() {
+app.listen(PORT, function() {
     console.log("Server is warmed up.");
 });
